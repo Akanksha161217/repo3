85fd355624ba08efee643ea8d5ae32a2da3cc048
unknown <abc@.com>
2024-06-26 11:59:30 +0530

    Successfull

---
+++ arr.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Test
+{
+	public static void main(String args[])
+	{
+		int arr[5];
+		int i,n,el;
+		Scanner sc=new Scanner(System.in);
+		el=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+			if(el==arr[i])
+		{
+			System.out.println("Possition"+(i+1));
+			break;
+		}
+		
+	}
+	if(i==5)
+	System.out.println("Element not Fount");
+}
